f000a47b14aa0d55e705d15f52f237800b1aec32
logonrm <logonrm@fiap.com.br>
2017-04-03 19:49:47 -0300

    read das categorias no BD

--- includes/navigation.php
+++ includes/navigation.php
@@ -14,7 +14,16 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li>
+
+                    <?php 
+                        global $connection;
+                        $query = "select * from categorias";
+                        $categorias = mysqli_query($connection, $query);
+                        while($cat = mysqli_fetch_assoc($categorias))
+                            echo "<li><a href='#'>".$cat['cat_nome']."</a></li>";
+                    ?>
+
+                    <!--<li>
                         <a href="#">About</a>
                     </li>
                     <li>
@@ -22,7 +31,7 @@
                     </li>
                     <li>
                         <a href="#">Contact</a>
-                    </li>
+                    </li>-->
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
